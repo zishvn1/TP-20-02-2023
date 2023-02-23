b3483bf40d07fa7d8b9beeb26a4562733742cedd
Avjit Basra <210073065@aston.ac.uk>
2023-02-23 16:14:54 +0000

    index and navbar update

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,19 +9,60 @@
 </head>
 
 <style>
-    p {
-        color:black;
+    .mainimg {
+        max-width: 100%;
+        max-height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        filter: blur(2px);
     }
-    h1{
+
+    .image-container {
+        position: relative;
+    }
+
+    .image-text-main {
+        position: absolute;
+        top: 20%;
+        left: 20%;
+        color: white;
+        font-size: 50px;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+
+    .image-text-sec {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 1;
+        color: white;
         text-align: center;
-        font-size: 10em;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     }
+
+    .highlight {
+        color: #bf1E2E;
+    }
+    
 </style>
 
 <body>
-    <?php include 'navbar.php'?>
+    <?php include 'navbar.php' ?>
 
-    <h1>Index</h1>
+    <div class="image-container">
+        <img src="images/indexbmw.png" class="mainimg">
+        <div class="image-text-main">
+            <p>We Provide <span class="highlight">Luxury Cars</span></p>
+            <p>at low cost.</p>
+        </div>
+        <div class="image-text-sec">
+            <p>SHOP BY BRANDS</p>
+        </div>
+    </div>
+    </div>
 
 </body>
+
 </html>
