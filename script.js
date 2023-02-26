344c2e968590019cabed37637b73f663fdcf2221
Shahed Abdulaleem <210040517@aston.ac.uk>
2023-02-26 16:42:27 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".container"),
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
