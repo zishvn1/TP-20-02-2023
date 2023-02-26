6ea8d6f7faa6e6a6ece0e74afb2b71704b18a94a
Shahed Abdulaleem <210040517@aston.ac.uk>
2023-02-26 17:29:30 +0000

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,11 @@ const container = document.querySelector(".container"),
             }) 
         })
     })
+
+    // js code to appear signup and login form
+    signUp.addEventListener("click", ( )=>{
+        container.classList.add("active");
+    });
+    login.addEventListener("click", ( )=>{
+        container.classList.remove("active");
+    });
