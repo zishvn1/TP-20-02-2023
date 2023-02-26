29dcd8ac4c09009072ec9178300b25e823a0c42a
Shahed Abdulaleem <210040517@aston.ac.uk>
2023-02-26 16:57:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const container = document.querySelector(".container"),
       pwFields = document.querySelectorAll(".password"),
       signUp = document.querySelector(".signup-link"),
       login = document.querySelector(".login-link");
+
+      //   js code to show/hide password and change icon
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", ()=>{
+            pwFields.forEach(pwField =>{
+                if(pwField.type ==="password"){
+                    pwField.type = "text";
