73e23a9fca87bfc6f452803018b23081430dc2af
zishvn1 <116391855+zishvn1@users.noreply.github.com>
2023-03-24 17:42:53 +0000

    Add files via upload

--- AstonAutosV3/connect.php
+++ AstonAutosV3/connect.php
@@ -2,7 +2,7 @@
 <?php
 
 //$con = new mysqli('localhost','u_160056307','WVYGb8D9cwqLUGs','u_160056307_db');
-$con = new mysqli('localhost','root','','team10');
+$con = new mysqli('localhost','root','','testing');
 
 if(!$con)
 {
