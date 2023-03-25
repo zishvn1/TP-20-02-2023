4a6fc5c3dd8f25143d29875cb80d7c10067776b1
Xavier Walker <210067271@aston.ac.uk>
2023-03-25 19:38:38 +0000

    should be all files

--- AstonAutosV3/AddAudiToBasket.php
+++ AstonAutosV3/AddAudiToBasket.php
@@ -1,3 +1,4 @@
+<!-- Code by Xavier Walker 210067271 -->
 <!DOCTYPE html>
 <?php
 session_start();
@@ -14,20 +15,32 @@ include("connect.php");
         }
     </style>
 </head>
+<?php
+// Gets the Audi's id  and passes it in the sql query to find the product 
+$id = $_GET['id'];
+$audisql = "SELECT audiid,Quantity FROM audiproducts WHERE audiid =$id";
+$audi_products = $con->query($audisql);
+$audi_products_row = mysqli_fetch_assoc($audi_products);
+// If the quantity(stock) of the car is more than 1 the user adds the product to their basket. And a message 'product added to cart' is displayed(technically the messaged is stored in the variable $message to be used later)
+// If the  user quantity requst is  greater than or equal to the quantity(stock) of the car is  the user adds the product to their basket. And a message 'product added to cart' is displayed(technically the messaged is stored in the variable $message to be used later)
+$_SESSION['AudiQuantity'] = $audi_products_row['Quantity'];
+$audiid = $audi_products_row['audiid'];
 
-<body>
-
-    <?php
-
-    //check if product is already in the cart
-
+if (!isset($Quantity)) {
+    $Quantity = count(array_keys($_SESSION['audicart'], $audiid));
+}
+if ($Quantity < $_SESSION['AudiQuantity']) {
     array_push($_SESSION['audicart'], $_GET['id']);
-    $_SESSION['message'] = 'Product added to cart';
+    $message = 'Product added to cart';
+    ++$Quantity;
+    //If the stock is <= 1 then the product is not added to the user's basket and a message 'out of stock' is displayed(technically the messaged is stored in the variable $message to be used later)
+} else {
+    $message = 'out of stock';
+    echo "<script>window.alert('Sorry, out of stock')</script>";
+}
+//This redirects the user back to the current basket page and the relevant message is displayed to the user
 
-
-    header("location:products.php")
-    ?>
-
-</body>
+header("location:products.php?message=" . urlencode($message));
+?>
 
 </html>
--- AstonAutosV3/AddMercedesToBasket.php
+++ AstonAutosV3/AddMercedesToBasket.php
@@ -5,30 +5,44 @@ session_start();
 include("connect.php");
 ?>
 <html>
+
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {font-family: Arial, Helvetica, sans-serif;}
-</style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
 </head>
+
 <body>
 
-<?php 
-$id=$_GET['id'];
-$mercedessql = "SELECT mercedesid,Quantity FROM mercedesproducts WHERE mercedesid =$id";
-$mercedes_products = $con->query($mercedessql);
-$mercedes_products_row = mysqli_fetch_assoc($mercedes_products);
-	if($mercedes_products_row['Quantity']>1){
-		array_push($_SESSION['mercedescart'], $_GET['id']);
-		$message = 'Product added to cart';
-		//echo $_SESSION['message'];
-	
- 
-	
-    }else{
-        $message ='out of stock';
-    echo"<script>window.alert('Sorry, out of stock')</script>";    
-    } header("location:products.php?message=".urlencode($message));
-?>
+    <?php
+    // Gets the Audi's id  and passes it in the sql query to find the product 
+
+    $id = $_GET['id'];
+    $mercedessql = "SELECT mercedesid,Quantity FROM mercedesproducts WHERE mercedesid =$id";
+    $mercedes_products = $con->query($mercedessql);
+    $mercedes_products_row = mysqli_fetch_assoc($mercedes_products);
+    // If the  user quantity requst is  greater than or equal to the quantity(stock) of the car is  the user adds the product to their basket. And a message 'product added to cart' is displayed(technically the messaged is stored in the variable $message to be used later)
+    $_SESSION['MercedesQuantity'] = $mercedes_products_row['Quantity'];
+    $mercedesid = $mercedes_products_row['mercedesid'];
+
+    if (!isset($Quantity)) {
+        $Quantity = count(array_keys($_SESSION['mercedescart'], $mercedesid));
+    }
+    if ($Quantity < $_SESSION['MercedesQuantity']) {
+        array_push($_SESSION['mercedescart'], $_GET['id']);
+        $message = 'Product added to cart';
+        ++$Quantity;
+    } else {
+        //If the stock is <= 1 then the product is not added to the user's basket and a message 'out of stock' is displayed(technically the messaged is stored in the variable $message to be used later)
+        $message = 'out of stock';
+        echo "<script>window.alert('Sorry, out of stock')</script>";
+    }
+    //This redirects the user back to the current basket page and the relevant message is displayed to the user
+    header("location:products.php?message=" . urlencode($message));
+    ?>
 </body>
+
 </html>
