5ba4a3ba3c0e885ded627fdbfc267e9089722344
Zishan Ashfaq <210131701@aston.ac.uk>
2023-03-25 03:10:30 +0000

    added css to the filter which makes it so much better

--- AstonAutosV3/filterNew.php
+++ AstonAutosV3/filterNew.php
@@ -3,17 +3,70 @@
   <style>
     .form-container {
         float: left;
-        width: 20%;
+        width: 13%;
         display: flex;
   flex-direction: column;
-  margin-top: 10%;
+  margin-top: 5%;
+  padding: 10px;
  
     }
+        .car-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 25px;
+  margin-left: 30%;
+  margin-top: 5%;
+}
+.labels {
+        display: block;
+        margin-bottom: 5px;
+        padding-top: 10px;
+      }
+   /* style the select elements */
+   .selects {
+        display: block;
+        width: 100%;
+        padding: 8px 16px;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        background-color: #fff;
+        font-size: 16px;
+        line-height: 1.5;
+        appearance: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+      }
+      .options {
+        font-size: 16px;
+        line-height: 1.5;
+      }
+
+      /* style the submit button */
+     .filter {
+        display: inline-block;
+        background-color: grey;
+        color: #fff;
+        padding: 12px 24px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        font-size: 16px;
+        margin-top: 20px;
+      }
+      .filter:active {
+  background-color: red;
+}
+
+.car-box {
+  background-color: #f5f5f5;
+  border: 1px solid #ddd;
+  padding: 10px;
+  text-align: center;
+  box-shadow: 0px 0px 5px #ddd;
+  margin-top: 10%;
+  margin-right: 3%;
+}
     
-    .car-container {
-        margin-left: 22%;
-    }
-</style>
 
   </style>
 </head>
@@ -25,48 +78,48 @@ changes inflicted on the variable names could lead to catastrophic outcomes to t
 <!-- one possible change that could be inflicted is getting the filter button to not reset the form each time it is clicked-->
   <body>
     <div class ="form-container">
-<form id = "filter-form" method="post" action="filterNewV3.php">
-  <label for="make">Make:</label>
-  <select name="make" id="make">
-    <option value="">Any</option>
-    <option value="Audi">Audi</option>
-    <option value="BMW">BMW</option>
-    <option value="Mercedes">Mercedes</option>
-    <option value="Toyota">Toyota</option>
-    <option value="Volkswagen">Volkswagen</option>
+<form id = "filter-form" method="post" action="filterNew.php">
+  <label class = "labels" for="make"></label>
+  <select class = "selects" name="make" id="make">
+  <option class="options" value="">Select Make</option>
+    <option class="options" value="">Any</option>
+    <option class="options" value="Audi">Audi</option>
+    <option class="options" value="BMW">BMW</option>
+    <option class="options" value="Mercedes">Mercedes</option>
+    <option class="options" value="Toyota">Toyota</option>
+    <option class="options" value="Volkswagen">Volkswagen</option>
   </select> <!-- this code is a simple form which takes all of the makes for the cars  -->
 
-  <label for="model">Model:</label>
-  <select name="model" id="model">
-    <option value="">Any</option>
-    <option value="A1">A1</option>
-    <option value="A3">A3</option>
-    <option value="A5">A5</option>
-    <option value="Q5">Q5</option>
-    <option value="Q7">Q7</option>
-    <option value="3 Series">3 Series</option>
-    <option value="5 Series">5 Series</option>
-    <option value="4 Series">4 Series</option>
-    <option value="X3">X3</option>
-    <option value="X5">X5</option>
-    <option value="A Class">A Class</option>
-    <option value="B Class">B Class</option>
-    <option value="C Class">C Class</option>
-    <option value="E Class">E Class</option>
-    <option value="S Class">S Class</option>
-    <option value="Yaris">Yaris</option>
-    <option value="Auris">Auris</option>
-    <option value="Prius">Prius</option>
-    <option value="Corolla">Corolla</option>
-    <option value="Avensis">Avensis</option>
-    <option value="Golf">Golf</option>
-    <option value="Passat">Passat</option>
-    <option value="Touran">Touran</option>
-    <option value="Up">Up</option>
-    <option value="Tiguan">Tiguan</option>
+  <label class = "labels" for="model"></label>
+  <select class = "selects" name="model" id="model">
+  <option class="options" value="">Select Model</option>
+    <option class="options" value="">Any</option>
+    <option class="options" value="A1">A1</option>
+    <option class="options" value="A3">A3</option>
+    <option class="options" value="A5">A5</option>
+    <option class="options" value="Q5">Q5</option>
+    <option class="options" value="Q7">Q7</option>
+    <option class="options" value="3 Series">3 Series</option>
+    <option class="options" value="5 Series">5 Series</option>
+    <option class="options" value="4 Series">4 Series</option>
+    <option class="options" value="X3">X3</option>
+    <option class="options" value="X5">X5</option>
+    <option class="options" value="A Class">A Class</option>
+    <option class="options" value="B Class">B Class</option>
+    <option class="options" value="C Class">C Class</option>
+    <option class="options" value="E Class">E Class</option>
+    <option class="options" value="S Class">S Class</option>
+    <option class="options" value="Yaris">Yaris</option>
+    <option class="options" value="Auris">Auris</option>
+    <option class="options" value="Prius">Prius</option>
+    <option class="options" value="Corolla">Corolla</option>
+    <option class="options" value="Avensis">Avensis</option>
+    <option class="options" value="Golf">Golf</option>
+    <option class="options" value="Passat">Passat</option>
+    <option class="options" value="Touran">Touran</option>
+    <option class="options" value="Up">Up</option>
+    <option class="options" value="Tiguan">Tiguan</option>
   </select> <!-- this code takes the models for each car. they yet have not been assigned to their respective make-->
-
-
   <script>/*this below code assigns each model to their respective make. in order to keep it logically correct.
   and to make sure that each model reflects their respective make when being filtered.*/
     const models = {//this tells the code what make each model should be assigned to
@@ -101,90 +154,89 @@ changes inflicted on the variable names could lead to catastrophic outcomes to t
   </script>
   <!-- this form below continues to take inputs from the user to filter the 
 price, year mileage, fueltype, drivetype(gearbox), color and condition -->
-
-<label for="price">Price:</label>
-  <select name="price" id="price">
-    <option value="">Any</option>
-    <option value="1">Less than £10,000</option>
-    <option value="2">£10,000 to £15,000</option>
-    <option value="3">£15,000 to £20,000</option>
-    <option value="4">£20,000 to £30,000</option>
-    <option value="5">£30,000 to £40,000</option>
-    <option value="6">£40,000 to £50,000</option>
-    <option value="7">£50,000 to £100,000</option>
+<label class = "labels" for="price"></label>
+  <select class = "selects" name="price" id="price">
+  <option class="options" value="">Select Price</option>
+    <option class="options" value="">Any</option>
+    <option class="options" value="1">Less than £10,000</option>
+    <option class="options" value="2">£10,000 to £15,000</option>
+    <option class="options" value="3">£15,000 to £20,000</option>
+    <option class="options" value="4">£20,000 to £30,000</option>
+    <option class="options" value="5">£30,000 to £40,000</option>
+    <option class="options" value="6">£40,000 to £50,000</option>
+    <option class="options" value="7">£50,000 to £100,000</option>
   </select>
-
-  <label for="year">Year:</label>
-<select name="year" id="year">
-  <option value="">Any</option>
-  <option value="2005-2010">2005 - 2010</option>
-  <option value="2010-2015">2010 - 2015</option>
-  <option value="2015-2020">2015 - 2020</option>
-  <option value="2020-newer">2020 or Newer</option>
+  <label class = "labels" for="year"></label>
+<select class = "selects" name="year" id="year">
+<option class="options" value="">Select Year</option>
+  <option class="options" value="">Any</option>
+  <option class="options" value="2005-2010">2005 - 2010</option>
+  <option class="options" value="2010-2015">2010 - 2015</option>
+  <option class="options" value="2015-2020">2015 - 2020</option>
+  <option class="options" value="2020-newer">2020 or Newer</option>
 </select>
-
-<label for="mileage">Mileage:</label>
-<select name="mileage" id="mileage">
-  <option value="">Any</option>
-  <option value="10000">Below 10,000</option>
-  <option value="15000">Below 15,000</option>
-  <option value="20000">Below 20,000</option>
-  <option value="25000">Below 25,000</option>
-  <option value="30000">Below 30,000</option>
-  <option value="35000">Below 35,000</option>
-  <option value="40000">Below 40,000</option>
-  <option value="45000">Below 45,000</option>
-  <option value="50000">Below 50,000</option>
-  <option value="55000">Below 55,000</option>
-  <option value="60000">Below 60,000</option>
-  <option value="65000">Below 65,000</option>
-  <option value="70000">Below 70,000</option>
-  <option value="75000">Below 75,000</option>
-  <option value="80000">Below 80,000</option>
-  <option value="85000">Below 85,000</option>
-  <option value="90000">Below 90,000</option>
-  <option value="95000">Below 95,000</option>
-  <option value="100000">Below 100,000</option>
+<label class = "labels" for="mileage"></label>
+<select class = "selects" name="mileage" id="mileage">
+<option class="options" value="">Select Mileage</option>
+  <option class="options" value="">Any</option>
+  <option class="options" value="10000">Below 10,000</option>
+  <option class="options" value="15000">Below 15,000</option>
+  <option class="options" value="20000">Below 20,000</option>
+  <option class="options" value="25000">Below 25,000</option>
+  <option class="options" value="30000">Below 30,000</option>
+  <option class="options" value="35000">Below 35,000</option>
+  <option class="options" value="40000">Below 40,000</option>
+  <option class="options" value="45000">Below 45,000</option>
+  <option class="options" value="50000">Below 50,000</option>
+  <option class="options" value="55000">Below 55,000</option>
+  <option class="options" value="60000">Below 60,000</option>
+  <option class="options" value="65000">Below 65,000</option>
+  <option class="options" value="70000">Below 70,000</option>
+  <option class="options" value="75000">Below 75,000</option>
+  <option class="options" value="80000">Below 80,000</option>
+  <option class="options" value="85000">Below 85,000</option>
+  <option class="options" value="90000">Below 90,000</option>
+  <option class="options" value="95000">Below 95,000</option>
+  <option class="options" value="100000">Below 100,000</option>
 </select>
-
-<label for="fueltype">Fuel Type:</label>
-<select name="fueltype" id="fueltype">
-  <option value="">Any</option>
-  <option value="petrol">Petrol</option>
-  <option value="diesel">Diesel</option>
-  <option value="electric">Electric</option>
+<label class = "labels" for="fueltype"></label>
+<select class = "selects" name="fueltype" id="fueltype">
+<option class="options" value="">Select Fuel type</option>
+  <option class="options" value="">Any</option>
+  <option class="options" value="petrol">Petrol</option>
+  <option class="options" value="diesel">Diesel</option>
+  <option class="options" value="electric">Electric</option>
 </select>
-
-<label for="drivetype">Gearbox:</label>
-<select name="drivetype" id="drivetype">
-  <option value="">Any</option>
-  <option value="manual">Manual</option>
-  <option value="automatic">Automatic</option>
+<label class = "labels" for="drivetype"></label>
+<select class = "selects" name="drivetype" id="drivetype">
+<option class="options" value="">Select gearbox type</option>
+  <option class="options" value="">Any</option>
+  <option class="options" value="manual">Manual</option>
+  <option class="options" value="automatic">Automatic</option>
 </select>
-
-<label for="color">Color:</label>
-<select name="color" id="color">
-  <option value="">Any</option>
-  <option value="green">Green</option>
-  <option value="black">Black</option>
-  <option value="red">Red</option>
-  <option value="blue">Blue</option>
-  <option value="white">White</option>
-  <option value="grey">Grey</option>
+<label class = "labels" for="color"></label>
+<select class = "selects" name="color" id="color">
+<option class="options" value="">Select color</option>
+  <option class="options" value="">Any</option>
+  <option class="options" value="green">Green</option>
+  <option class="options" class="options"option class="options"ion value="black">Black</option>
+  <option class="options" value="red">Red</option>
+  <option class="options" value="blue">Blue</option>
+  <option class="options" value="white">White</option>
+  <option class="options" value="grey">Grey</option>
 </select>
-
-<label for="conditionofcar">Condition:</label>
-<select name="conditionofcar" id="conditionofcar">
-  <option value="">Any</option>
-  <option value="new">New</option>
-  <option value="used">Used</option>
+<label class = "labels" for="conditionofcar"></label>
+<select class = "selects" name="conditionofcar" id="conditionofcar">
+<option class="options" value="">Select condition</option>
+  <option class="options" value="">Any</option>
+  <option class="options" value="new">New</option>
+  <option class="options" value="used">Used</option>
 </select>
-<button type="submit" name="action" value="Filter">Filter</button>
+<button class="filter" type="submit" name="action" value="Filter">Submit</button>
 </form>
-  </div>
   </body>
 </html>
-
+  </div>
 <?php
 //connects to database, if theres any issues kill the connection and throw the sql error
 include ("connect.php");
@@ -354,24 +406,7 @@ $result = $con->query($sql);
 
 // display the car data in html format
 ?>
-<style>
-.car-container {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 25px;
-  margin-left: 20%;
-}
 
-.car-box {
-  background-color: #f5f5f5;
-  border: 1px solid #ddd;
-  padding: 10px;
-  text-align: center;
-  box-shadow: 0px 0px 5px #ddd;
-  margin-top: 10%;
-  margin-right: 3%;
-}
-</style>
 
 <div class="car-container">
   <?php
