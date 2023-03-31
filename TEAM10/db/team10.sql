1423e78f3ae9e83d08de27e06ee73f7a28d08582
Oluwatomisin Ogunnusi <210194658@aston.ac.uk>
2023-03-31 12:17:44 +0100

    team10.sql

--- TEAM10/db/team10.sql
+++ TEAM10/db/team10.sql
@@ -82,3 +82,48 @@ CREATE TABLE `reviews` (
   `Review` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL,
   `Star Count` int(11) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `Id` int(11) NOT NULL,
+  `Vehicle Id` int(11) NOT NULL,
+  `Product descr` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `Admin Id` int(11) NOT NULL,
+  `Order Id` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- Constraints for table `basket`
+--
+ALTER TABLE `basket`
+  ADD CONSTRAINT `basket_ibfk_1` FOREIGN KEY (`Customer Id`) REFERENCES `customers` (`Id`),
+  ADD CONSTRAINT `basket_ibfk_2` FOREIGN KEY (`Product Id`) REFERENCES `products` (`Id`);
+
+--
+-- Constraints for table `enquiries`
+--
+ALTER TABLE `enquiries`
+  ADD CONSTRAINT `enquiries_ibfk_1` FOREIGN KEY (`Customer Id`) REFERENCES `customers` (`Id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`Customer Id`) REFERENCES `customers` (`Id`);
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`Customer Id`) REFERENCES `customers` (`Id`),
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`Product Id`) REFERENCES `products` (`Id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
